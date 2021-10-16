5e0408745f03041eef06348ff353df33259a8bed
Jelizaveta Mahrova <Bonifazyy@yandex.ru>
2021-10-16 21:18:49 +0300

    fixed

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const indexRouter = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const limiter = require('./utils/rateLimit');
 const { PORTSERVER, ADRESS, NODE_ENV } = require('./utils/constants');
 const errorsHandler = require('./middlewares/errors');
+const cors = require('cors');
 
 const app = express();
 const { PORT = PORTSERVER } = process.env;
@@ -19,9 +19,17 @@ mongoose.connect(adressBD, {
   useFindAndModify: false,
 });
 
+app.options('*', cors());
+
+app.use(cors({
+  origin: ['http://elmovies.nomoredomains.monster', 'https://elmovies.nomoredomains.monster', 'http://localhost:3000', 'https://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 app.use(requestLogger);
 app.use(express.json());
-app.use(limiter);
 app.use(helmet());
 
 app.use('/', indexRouter);
